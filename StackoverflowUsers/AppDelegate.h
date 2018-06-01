8a51fe0b87c73a0502c91f3cef9fb8f1a873380d
jian <jiantutk@gmail.com>
2018-06-01 17:21:23 -0400

    Initial Commit

---
+++ StackoverflowUsers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackoverflowUsers
+//
+//  Created by Jian on 6/1/18.
+//  Copyright © 2018 Jian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StackoverflowUsers/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StackoverflowUsers
+//
+//  Created by Jian on 6/1/18.
+//  Copyright © 2018 Jian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
